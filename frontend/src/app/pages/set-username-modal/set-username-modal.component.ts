224d036496f9a898a9c0a143e7bb6d6120cc9dcb
zenzen <song8713@qq.com>
2022-07-05 12:34:55 +0800

    feat：支持掉线重登

--- frontend/src/app/pages/set-username-modal/set-username-modal.component.ts
+++ frontend/src/app/pages/set-username-modal/set-username-modal.component.ts
@@ -10,6 +10,10 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class SetUsernameModalComponent implements OnInit {
   isVisible: any;
   usernameForm: FormGroup;
+  hasOldUser: any;
+  okText: any;
+  usernameFormOld: any;
+  cancelText: any;
 
   constructor(public playerService: PlayerService, private fb: FormBuilder) {
     this.isVisible = true;
@@ -17,6 +21,15 @@ export class SetUsernameModalComponent implements OnInit {
       username: [null, Validators.required],
       roomId: [null, Validators.required]
     });
+
+    this.cancelText = '使用上次用户';
+    this.usernameFormOld = {
+      username: localStorage.getItem('oldUserName'),
+      roomId: localStorage.getItem('oldUserRoomId')
+    };
+    this.hasOldUser = !!localStorage.getItem('oldUserName');
+
+    this.okText = '确定' + (this.hasOldUser ? '（点击关闭使用上一个用户）' : '');
   }
 
   ngOnInit(): void {
@@ -24,7 +37,9 @@ export class SetUsernameModalComponent implements OnInit {
 
 
   handleOk(value: any): void {
-    if (this.usernameForm.valid){
+    if (value.username && value.roomId){
+      localStorage.setItem('oldUserName', value.username);
+      localStorage.setItem('oldUserRoomId', value.roomId);
       this.playerService.username = value.username;
       this.playerService.roomId = value.roomId;
       this.isVisible = false;
